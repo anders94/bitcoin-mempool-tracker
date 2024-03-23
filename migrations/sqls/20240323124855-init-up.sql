dfbdc205211b71a74d44eb367500c1fca7359dc4
Anders Brownworth <anders-github@evantide.com>
2024-03-23 11:02:37 -0400

    record mempool entry and exit - entry is null if we don't know it

--- migrations/sqls/20240323124855-init-up.sql
+++ migrations/sqls/20240323124855-init-up.sql
@@ -1,11 +1,11 @@
 -- --------------------------------------------------------
 -- -- Table: txs
 -- --------------------------------------------------------
-
 CREATE TABLE txs (
   txid               TEXT            NOT NULL UNIQUE,
-  mempool_date       TIMESTAMP,
-  block_date         TIMESTAMP,
+  mempool_entry      TIMESTAMP,
+  mempool_exit       TIMESTAMP,
+  block_entry        TIMESTAMP,
   value_in           BIGINT,
   value_out          BIGINT,
   bytes              INT
@@ -14,7 +14,6 @@ CREATE TABLE txs (
 -- --------------------------------------------------------
 -- -- Table: txos
 -- --------------------------------------------------------
-
 CREATE TABLE txos (
   txid               TEXT            NOT NULL,
   idx                INT             NOT NULL,
