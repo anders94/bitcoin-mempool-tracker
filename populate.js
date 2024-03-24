df0608c5db6df0289c81733899b0d1a99704805c
Anders Brownworth <anders-github@evantide.com>
2024-03-24 19:23:54 -0400

    populate: loop endlessly with a short and long delay

--- populate.js
+++ populate.js
@@ -3,7 +3,8 @@ const util = require('util');
 const config = require('./config');
 const db = require('./db');
 
-const delay = 100;
+const shortDelay = 100;
+const longDelay = 10 * 1000;
 
 const rpc = new RpcClient({
     protocol: config.rpc.protocol,
@@ -58,56 +59,60 @@ const decodeRawTransaction = async (tx) => {
 };
 
 (async () => {
-    const txRes = await db.query('SELECT txid FROM txs WHERE bytes IS NULL');
-    for (let x = 0; x < txRes.rows.length; x++) {
-	const txid = txRes.rows[x].txid;
-	console.log(txid);
-	const rtx = await getRawTransaction(txid);
-	if (rtx) {
-	    const tx = await decodeRawTransaction(rtx);
+    while (true) {
+	const txRes = await db.query('SELECT txid FROM txs WHERE bytes IS NULL');
+	for (let x = 0; x < txRes.rows.length; x++) {
+	    const txid = txRes.rows[x].txid;
+	    console.log(txid);
+	    const rtx = await getRawTransaction(txid);
+	    if (rtx) {
+		const tx = await decodeRawTransaction(rtx);
 
-	    // https://en.bitcoin.it/wiki/Weight_units
-	    console.log((x+1) + ' of ' + txRes.rows.length + ' = ' + ((x/txRes.rows.length) * 100).toFixed(2) + '%');
+		// https://en.bitcoin.it/wiki/Weight_units
+		console.log((x+1) + ' of ' + txRes.rows.length + ' = ' + ((x/txRes.rows.length) * 100).toFixed(2) + '%');
 
-	    console.log(txid, 'size', tx.size, 'vsize', tx.vsize, 'weight', tx.weight)
-	    //console.log(util.inspect(tx, {depth: 6}));
+		console.log(txid, 'size', tx.size, 'vsize', tx.vsize, 'weight', tx.weight)
+		//console.log(util.inspect(tx, {depth: 6}));
 
-	    // go through the vins and find the value of each output
-	    let inval = 0n;
-	    for (let v = 0; v < tx.vin.length; v++) {
-		const vin = tx.vin[v];
-		const vinrtx = await getRawTransaction(vin.txid);
-		const vintx = await decodeRawTransaction(vinrtx);
+		// go through the vins and find the value of each output
+		let inval = 0n;
+		for (let v = 0; v < tx.vin.length; v++) {
+		    const vin = tx.vin[v];
+		    const vinrtx = await getRawTransaction(vin.txid);
+		    const vintx = await decodeRawTransaction(vinrtx);
 
-		let val = 0;
-		for (let w = 0; w < vintx.vout.length; w++) {
-		    if (vintx.vout[w].n == vin.vout) {
-			val = toInt(vintx.vout[w].value);
-			await db.query('INSERT InTO txos (txid, idx, amount) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING', [vin.txid, vintx.vout[w].n, val]);
+		    let val = 0;
+		    for (let w = 0; w < vintx.vout.length; w++) {
+			if (vintx.vout[w].n == vin.vout) {
+			    val = toInt(vintx.vout[w].value);
+			    await db.query('INSERT InTO txos (txid, idx, amount) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING', [vin.txid, vintx.vout[w].n, val]);
+
+			}
 
 		    }
+		    inval += val;
+		    console.log(' ', vin.txid, '+', val.toString());
 
 		}
-		inval += val;
-		console.log(' ', vin.txid, '+', val.toString());
+		// add up outputs
+		let outval = 0n;
+		for (let v = 0; v < tx.vout.length; v++) {
+		    console.log(' ', txid, '-', toInt(tx.vout[v].value).toString());
+		    outval += toInt(tx.vout[v].value);
 
-	    }
-	    // add up outputs
-	    let outval = 0n;
-	    for (let v = 0; v < tx.vout.length; v++) {
-		console.log(' ', txid, '-', toInt(tx.vout[v].value).toString());
-		outval += toInt(tx.vout[v].value);
+		}
+		const fee = inval - outval;
 
-	    }
-	    const fee = inval - outval;
+		db.query('UPDATE txs SET value_in = $1, value_out = $2, bytes = $3 WHERE txid = $4', [inval, outval, tx.weight, txid]);
 
-	    db.query('UPDATE txs SET value_in = $1, value_out = $2, bytes = $3 WHERE txid = $4', [inval, outval, tx.weight, txid]);
-
-	    console.log('  fee ------------------------------------------------------------ =', fee.toString());
-	    console.log();
+		console.log('  fee ------------------------------------------------------------ =', fee.toString());
+		console.log();
 	    
+	    }
+	    await sleep(shortDelay);
+
 	}
-	await sleep(delay);
+	await sleep(longDelay);
 
     }
 
